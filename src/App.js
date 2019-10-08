610edb6da7d12271b32d6b1264916468e9f24afd
yosi-i <ishihara.yoshihisa@dentsudigital.co.jp>
2019-10-08 20:52:27 +0900

    [更新] テキストを更新した際にStateを更新するように改修

--- src/App.js
+++ src/App.js
@@ -9,24 +9,38 @@ function cleanPaste(e) {
   document.execCommand('insertHTML', false, text)
 }
 
-function updateCell(updatef, rowid, e) {
-  
-}
-
 function Cell(prop) {
-  // const [isInput,setIsInput] = useState(false);
   const type = prop.type
-  const setRow = prop.setRow
+  const itemid = prop.itemid
+  const rowid = prop.rowid
+  const table = prop.table.slice()
+  const setTable = prop.setTable
+
+  const updateCell = e => {
+    const node = e.target.nodeName
+    const newtable = table.map(v => {
+      if (v.id === rowid || node === 'TD') {
+        v.data[itemid] = e.target.innerHTML
+        return v
+      } else if (v.id === rowid || node === 'INPUT') {
+        v.data[itemid] = e.target.value
+        return v
+      } else {
+        return v
+      }
+    })
+    setTable(newtable)
+  }
 
   if (type === 'bool') {
     return (
       <td>
-        <input type="checkbox" value={prop.content} />
+        <input type="checkbox" value={prop.content || false} onChange={e => updateCell(e)} />
       </td>
     )
   } else {
     return (
-      <td contentEditable={true} onPaste={cleanPaste} onBlur={e => updateCell(setRow, prop.rowid,e)}>
+      <td contentEditable={true} onPaste={cleanPaste} onBlur={e => updateCell(e)}>
         {prop.content}
       </td>
     )
@@ -38,7 +52,14 @@ function Row(prop) {
   return (
     <tr>
       {items.map((item, i) => (
-        <Cell key={item.id} rowid={prop.rowid} type={item.type} content={r[item.id] || null} setRow={prop.setRow} />
+        <Cell
+          key={item.id}
+          rowid={prop.rowid}
+          itemid={item.id}
+          type={item.type}
+          content={r[item.id] || null}
+          {...prop}
+        />
       ))}
     </tr>
   )
@@ -46,51 +67,58 @@ function Row(prop) {
 
 function Rows(prop) {
   return (
-    <>
-      {prop.row.map(r => (
-        <Row key={r.id} rowid={r.id} data={r} setRow={prop.setRow} />
+    <tbody>
+      {prop.table.map(r => (
+        <Row key={r.id} rowid={r.id} data={r} {...prop} />
       ))}
-    </>
+    </tbody>
   )
 }
 
 function Header(prop) {
   return (
-    <tr>
-      {items.map(v => (
-        <th key={v.id}>{v.name}</th>
-      ))}
-    </tr>
+    <thead>
+      <tr>
+        {items.map(v => (
+          <th key={v.id}>{v.name}</th>
+        ))}
+      </tr>
+    </thead>
   )
 }
 
 function NewLineAdd(prop) {
+  const addRow = () => {
+    const r = prop.table.slice()
+    r.push({ id: shortid.generate(), data: {} })
+    prop.setTable(r)
+  }
+
   return (
-    <tr className="App-addrow">
-      <td
-        colSpan={items.length}
-        onClick={() => {
-          const r = prop.row.slice()
-          r.push({ id: shortid.generate() })
-          prop.setRow(r)
-        }}
-      >
-        ここをクリックしてね
-      </td>
-    </tr>
+    <tfoot className="App-addrow">
+      <tr>
+        <td colSpan={items.length} onClick={addRow}>
+          ここをクリックしてね
+        </td>
+      </tr>
+    </tfoot>
   )
 }
 
 function App() {
-  const [row, setRow] = useState([])
+  const [table, setTable] = useState([])
+  const prop = {
+    table: table,
+    setTable: setTable,
+  }
 
   return (
     <div className="App">
       <div className="App-title">テーブル設計サイト</div>
       <table className="App-table">
         <Header />
-        <Rows row={row} setRow={setRow} />
-        <NewLineAdd row={row} setRow={setRow} />
+        <Rows {...prop} />
+        <NewLineAdd {...prop} />
       </table>
     </div>
   )
