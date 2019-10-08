093d08b04e57bc0f2d4f4bf108d61be7f81bb612
yosi-i <ishihara.yoshihisa@dentsudigital.co.jp>
2019-10-08 18:27:56 +0900

    [更新] 簡易的な枠組みを作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
 import React, { useState } from 'react'
+import shortid from 'shortid'
 import './App.scss'
+import { items } from './definition'
 
-const items = [
-  { name: 'カラム名', type: 'text' },
-  { name: '論理名', type: 'alphabet' },
-  { name: 'is PK', type: 'bool' },
-  { name: 'データ元', type: 'text' },
-]
+function cleanPaste(e) {
+  e.preventDefault()
+  var text = (e.originalEvent || e).clipboardData.getData('text/plain')
+  document.execCommand('insertHTML', false, text)
+}
+
+function updateCell(updatef, rowid, e) {
+  
+}
+
+function Cell(prop) {
+  // const [isInput,setIsInput] = useState(false);
+  const type = prop.type
+  const setRow = prop.setRow
+
+  if (type === 'bool') {
+    return (
+      <td>
+        <input type="checkbox" value={prop.content} />
+      </td>
+    )
+  } else {
+    return (
+      <td contentEditable={true} onPaste={cleanPaste} onBlur={e => updateCell(setRow, prop.rowid,e)}>
+        {prop.content}
+      </td>
+    )
+  }
+}
 
 function Row(prop) {
+  const r = prop.data
+  return (
+    <tr>
+      {items.map((item, i) => (
+        <Cell key={item.id} rowid={prop.rowid} type={item.type} content={r[item.id] || null} setRow={prop.setRow} />
+      ))}
+    </tr>
+  )
+}
+
+function Rows(prop) {
   return (
     <>
       {prop.row.map(r => (
-        <tr key={r[0]}>
-          {r.map(i => (
-            <td key={i}>{i}</td>
-          ))}
-        </tr>
+        <Row key={r.id} rowid={r.id} data={r} setRow={prop.setRow} />
       ))}
     </>
+  )
+}
+
+function Header(prop) {
+  return (
+    <tr>
+      {items.map(v => (
+        <th key={v.id}>{v.name}</th>
+      ))}
+    </tr>
+  )
+}
+
+function NewLineAdd(prop) {
+  return (
+    <tr className="App-addrow">
+      <td
+        colSpan={items.length}
+        onClick={() => {
+          const r = prop.row.slice()
+          r.push({ id: shortid.generate() })
+          prop.setRow(r)
+        }}
+      >
+        ここをクリックしてね
+      </td>
+    </tr>
   )
 }
 
@@ -27,26 +86,11 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App-title">テーブルを作るよ</div>
+      <div className="App-title">テーブル設計サイト</div>
       <table className="App-table">
-        <tr>
-          {items.map(v => (
-            <th key={v.name}>{v.name}</th>
-          ))}
-        </tr>
-        <Row row={row} />
-        <tr className="App-addrow">
-          <td
-            onClick={() => {
-              const r = row.slice()
-              const i = row.length;
-              r.push(['ああ'+i, 'いい'+i, 'うう'+i, 'ええ'+i])
-              setRow(r)
-            }}
-          >
-            ここをクリックしてね
-          </td>
-        </tr>
+        <Header />
+        <Rows row={row} setRow={setRow} />
+        <NewLineAdd row={row} setRow={setRow} />
       </table>
     </div>
   )
---
+++ src/definition.js
@@ -0,0 +1,23 @@
+const items = [
+  { id: 'name', name: 'カラム名', type: 'text' },
+  { id: 'logicName', name: '論理名', type: 'alphabet' },
+  { id: 'type', name: '型', type: 'type-select' },
+  { id: 'supplementation', name: '補足', type: 'textarea' },
+  { id: 'calc', name: '計算ロジック', type: 'textarea' },
+  { id: 'ispk', name: 'is PK', type: 'bool' },
+  { id: 'isnull', name: 'nullを許容', type: 'bool' },
+  { id: 'defaultvalue', name: 'デフォルト値', type: 'text' },
+  { id: 'from', name: 'データ元', type: 'text' },
+  { id: 'example', name: '項目例', type: 'textarea' },
+]
+
+const columnTypes = [
+  { id: 'int', name: 'int' },
+  { id: 'double', name: 'double' },
+  { id: 'string', name: 'string' },
+  { id: 'varchar', name: 'varchar' },
+  { id: 'date', name: 'date' },
+  { id: 'datetime', name: 'datetime' },
+]
+
+export { items, columnTypes }
