2ecc3855103b96d357cda3678c10d1adc934728a
Yosi <7910558+tochiji@users.noreply.github.com>
2019-10-11 01:51:59 +0900

    [修正] Drag & Dropを実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,134 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useRef } from 'react'
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 import shortid from 'shortid'
 import './App.scss'
 import { items } from './definition'
 import { save, load } from './save'
-import { CellBool, CellTypeSelect ,CellText } from './cells'
+import DnDTypes from './DnDTypes'
+import { CellBool, CellTypeSelect, CellText } from './cells'
 
+function App() {
+  const saved = load() || []
+  const [table, setTable] = useState(saved)
+  const prop = {
+    table: table,
+    setTable: setTable,
+  }
+
+  return (
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <div className="App-title">テーブル設計サイト</div>
+        <table className="App-table">
+          <Header />
+          <Rows {...prop} />
+          <NewLineAdd {...prop} />
+        </table>
+        <Info table={table} />
+      </DndProvider>
+    </div>
+  )
+}
+
+function Header(prop) {
+  const style = { backgroundColor: '#666', color: '#fff' }
+  return (
+    <thead>
+      <tr >
+        <th style={style}>No</th>
+        {items.map(v => (
+          <th key={v.id} style={style}>{v.name}</th>
+        ))}
+        <th className="del" style={{ backgroundColor: 'transparent' }} />
+      </tr>
+    </thead>
+  )
+}
+
+function Rows(prop) {
+  const table = prop.table
+
+  const moveRow = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragRow = table[dragIndex]
+      const hoverRow = table[hoverIndex]
+      const newTable = table.slice()
+      newTable[hoverIndex] = dragRow
+      newTable[dragIndex] = hoverRow
+      prop.setTable(newTable)
+      save(newTable)
+    },
+    [prop, table]
+  )
+
+  return (
+    <tbody>
+      {prop.table.map((r, i) => (
+        <Row key={r.id} rowid={r.id} rowNo={i} row={r} moveRow={moveRow} {...prop} />
+      ))}
+    </tbody>
+  )
+}
+
+function Row(prop) {
+  const ref = useRef(null)
+  const [, drop] = useDrop({
+    accept: DnDTypes.ROW,
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      console.log(ref)
+      const dragIndex = item.rowNo
+      const hoverIndex = prop.rowNo
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      const hoverBoundingRect = ref.current.getBoundingClientRect()
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      const clientOffset = monitor.getClientOffset()
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      prop.moveRow(dragIndex, hoverIndex)
+      item.rowNo = hoverIndex
+    },
+  })
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: DnDTypes.ROW, rowid: prop.rowid, rowNo: prop.rowNo },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  drag(drop(ref))
+
+  const del = () => {
+    const table = prop.table.slice()
+    const newtable = table.filter(r => {
+      return r.id !== prop.rowid
+    })
+    prop.setTable(newtable)
+  }
+  const r = prop.row.data
+  return (
+    <tr ref={ref} style={{ backgroundColor: isDragging ? '#eee' : '#fff' }}>
+      <td>{prop.rowNo + 1}</td>
+      {items.map((item, i) => (
+        <Cell key={item.id} rowid={prop.rowid} itemid={item.id} type={item.type} content={r[item.id]} {...prop} />
+      ))}
+      <td className="del" onClick={del} style={{ visibility: isDragging ? "hidden" : "display" }}>
+        del
+      </td>
+    </tr>
+  )
+}
 
 function Cell(prop) {
   if (prop.type === 'bool') {
@@ -14,50 +138,6 @@ function Cell(prop) {
   } else {
     return <CellText {...prop} />
   }
-}
-
-function Row(prop) {
-  const del = () => {
-    const table = prop.table.slice()
-    const newtable = table.filter(r =>{
-      return r.id !== prop.rowid
-    })
-    prop.setTable(newtable)
-  }
-  const r = prop.row.data
-  return (
-    <tr>
-      <td>{prop.rowNo}</td>
-      {items.map((item, i) => (
-        <Cell key={item.id} rowid={prop.rowid} itemid={item.id} type={item.type} content={r[item.id]} {...prop} />
-      ))}
-      <td className="del" onClick={del}>del</td>
-    </tr>
-  )
-}
-
-function Rows(prop) {
-  return (
-    <tbody>
-      {prop.table.map((r, i) => (
-        <Row key={r.id} rowid={r.id} rowNo={i+1} row={r} {...prop} />
-      ))}
-    </tbody>
-  )
-}
-
-function Header(prop) {
-  return (
-    <thead>
-      <tr>
-        <th>No</th>
-        {items.map(v => (
-          <th key={v.id}>{v.name}</th>
-        ))}
-        <th className="del" />
-      </tr>
-    </thead>
-  )
 }
 
 function NewLineAdd(prop) {
@@ -80,33 +160,12 @@ function NewLineAdd(prop) {
   )
 }
 
-function App() {
-  const saved = load() || []
-  const [table, setTable] = useState(saved)
-  const prop = {
-    table: table,
-    setTable: setTable,
-  }
-
-  return (
-    <div className="App">
-      <div className="App-title">テーブル設計サイト</div>
-      <table className="App-table">
-        <Header />
-        <Rows {...prop} />
-        <NewLineAdd {...prop} />
-      </table>
-      <Info table={table} />
-    </div>
-  )
-}
-
 function Info(prop) {
   const PKcount = prop.table.filter(v => {
     return v.data.ispk
   }).length
   return (
-    <div>
+    <div style={{ marginTop: '50px' }}>
       <div>{prop.table.length + '行あるよ'}</div>
       <div>{'PKは' + PKcount + '個あるよ'}</div>
     </div>
