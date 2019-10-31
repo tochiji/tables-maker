6a287ee8a2d511732b0791514c66606fc90c8a32
Yosi <7910558+tochiji@users.noreply.github.com>
2019-11-01 02:19:43 +0900

    [作成] ユーザー追加用APIを追加

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,69 @@
-const process = require('process'); // Allow env variable mocking
-const mysql = require('mysql');
+const process = require('process') // Allow env variable mocking
+const mysql = require('mysql')
 
-const connectionName =
-  process.env.INSTANCE_CONNECTION_NAME;
-const dbUser = process.env.SQL_USER;
-const dbPassword = process.env.SQL_PASSWORD;
-const dbName = process.env.DB_NAME;
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(bodyParser.json())
+
+const connectionName = process.env.INSTANCE_CONNECTION_NAME
+const dbUser = process.env.SQL_USER
+const dbPassword = process.env.SQL_PASSWORD
+const dbName = process.env.DB_NAME
 
 const mysqlConfig = {
   connectionLimit: 1,
   user: dbUser,
   password: dbPassword,
   database: dbName,
-};
-
-if (process.env.NODE_ENV === 'production') {
-  mysqlConfig.socketPath = `/cloudsql/${connectionName}`;
 }
 
-let mysqlPool;
+if (process.env.NODE_ENV === 'production') {
+  mysqlConfig.socketPath = `/cloudsql/${connectionName}`
+}
 
-exports.mysqlDemo = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+let mysqlPool
 
-  if (!mysqlPool) {
-    mysqlPool = mysql.createPool(mysqlConfig);
-  }
+app.get('/users', (req, res) => {
+  if (!mysqlPool) mysqlPool = mysql.createPool(mysqlConfig)
 
   mysqlPool.query('select * from users', (err, results) => {
+    res.setHeader('Content-Type', 'application/json')
     if (err) {
-      console.error(err);
-      res.status(500).send(err);
+      console.error(err)
+      res.status(500).send(err)
     } else {
-      res.status(200).send(JSON.stringify(results));
+      res.status(200).send(JSON.stringify(results))
     }
-  });
-};
+  })
+})
+
+app.post('/user', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+
+  if (!mysqlPool) mysqlPool = mysql.createPool(mysqlConfig)
+
+  const name = req.body.name
+  mysqlPool.query(`INSERT INTO users (name) VALUES('${name}');`, (err, results) => {
+    res.setHeader('Content-Type', 'application/json')
+    if (err) {
+      console.error(err)
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(JSON.stringify(results))
+    }
+  })
+})
+
+app.get('/test', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send({ message: 'testです' })
+})
+
+module.exports = { api: app }
